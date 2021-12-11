97ae957146a3ea66d1c7ac2b4161c6363b075efa
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-12 00:03:38 +0200

    Hero

--- js/app.js
+++ js/app.js
@@ -26,25 +26,7 @@ TweenMax.to(".third", 1.5, {
   ease: Expo.easeInOut,
 });
 
-//Hero animation
-// let tl = gsap.timeline({
-//   delay: 0.1,
-//   defaults: {
-//     duration: 1,
-//     ease: Power1.easeOut,
-//   },
-//   smoothChildTiming: true,
-// });
-// tl.from(".logo", { duration: 0.8, y: -500 })
-//   .from(".anchor-container", { duration: 1, y: -1000, delay: 0.02 })
-//   .from(".title", { duration: 0.8, delay: 0.01, y: -500 })
-//   .from(".subtitle", { duration: 0.8, delay: 0.02, y: -600 })
-//   .from(".caption", { duration: 0.8, delay: 0.02, y: -650 })
-//   .from(".hero .text", { duration: 0.8, delay: 0.02, y: -650 })
-//   .from(".link-1", { duration: 0.8, y: 400 }, "-=3")
-//   .from(".link-2", { duration: 0.8, y: 400 }, "-=2")
-//   .from(".link-3", { duration: 0.8, y: 400 }, "-=1")
-//   .from(".down-btn", { duration: 0.8, scale: 0, delay: 0.5 });
+
 gsap.from(".title", 1, {
   y: "-500",
   ease: Expo.easeInOut,
