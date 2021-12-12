83be39b45047badb1548d335a4244765585023d0
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-12 17:09:36 +0200

    Intro animation

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ new hoverEffect({
   image1:
     "https://res.cloudinary.com/du9veyxbv/image/upload/v1639321351/silver-anchor_lszmrb.png",
   image2:
-    "../img/wheel.png",
-  displacementImage:
     "https://res.cloudinary.com/du9veyxbv/image/upload/v1639321358/wheel_mxu7ki.png",
+  displacementImage:
+    "https://res.cloudinary.com/du9veyxbv/image/upload/v1639294332/8_lug9kb.jpg",
 });
 // GSAP
 
