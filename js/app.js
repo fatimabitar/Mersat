602439f48d284b123b6ab18deb0a0e6a8ba6d5bc
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-15 14:21:49 +0200

    edits

--- js/app.js
+++ js/app.js
@@ -202,3 +202,27 @@ const down = document.querySelector(".down-btn");
 down.addEventListener("click", () => {
   document.querySelector(".intro").scrollIntoView({ behavior: "smooth" });
 });
+
+//back to top
+let backToTop = document.getElementById("toTopButton");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) {
+    //Show Back To Top button
+    backToTop.style.transform = "scale(1)";
+  } else {
+    // Hide Back To Top button
+    backToTop.style.transform = "scale(0)";
+  }
+}
+
+backToTop.onclick = () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+console.log("555555555555555555");
