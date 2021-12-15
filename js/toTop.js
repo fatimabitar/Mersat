2c485181d59fa065f2025d11118b29cada38a03d
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-15 15:37:08 +0200

    edits

---
+++ js/toTop.js
@@ -0,0 +1,22 @@
+//back to top
+let backToTop = document.getElementById("toTopButton");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 500) {
+    //Show Back To Top button
+    backToTop.style.transform = "scale(1)";
+  } else {
+    // Hide Back To Top button
+    backToTop.style.transform = "scale(0)";
+  }
+}
+
+backToTop.onclick = () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
