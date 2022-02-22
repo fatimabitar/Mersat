08face07592ca472c7f5a305411fc39e37f4b1f8
Fatima Bitar <fatimabitar97@gmail.com>
2022-02-22 13:06:49 +0200

    printers images

--- js/hoverEffect.js
+++ js/hoverEffect.js
@@ -3,9 +3,9 @@ new hoverEffect({
   parent: document.querySelector(".img-holder"),
   intensity: 0.1,
   image1:
-    "https://res.cloudinary.com/du9veyxbv/image/upload/v1645526093/1-png_jdyzcx.png",
-  image2:
     "https://res.cloudinary.com/du9veyxbv/image/upload/v1645526136/2-png_mp4w4g.png",
+  image2:
+    "https://res.cloudinary.com/du9veyxbv/image/upload/v1645526093/1-png_jdyzcx.png",
   displacementImage:
     "https://res.cloudinary.com/du9veyxbv/image/upload/v1639294332/8_lug9kb.jpg",
 });
